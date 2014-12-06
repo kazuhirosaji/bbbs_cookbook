14c4e2d4afc4666f31aba655fc4aa8daf164d047
kazuhirosaji <kazuhirosaji@gmail.com>
2014-12-06 22:37:09 +0900

    set mysql password

--- cookbooks/mysql/recipes/default.rb
+++ cookbooks/mysql/recipes/default.rb
@@ -6,10 +6,22 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+execute "setpassword" do
+	update = "UPDATE user SET Password='' WHERE Host='localhost' and User='root';"
+	command "sudo /etc/init.d/mysql stop"
+	command "sudo killall mysqld"
+	command "sudo /usr/bin/mysqld_safe -u root --skip-grant-tables &"
+	command "mysqladmin -e #{update}"
+	command "mysqladmin -e FLUSH PRIVILEGES;"
+	command "/usr/bin/mysqladmin -u root password root"
+	action :nothing
+end
+
 package "mysql-server" do
   action :install
 end
 
 service "mysql" do
   action [ :enable, :start]
+  notifies :run, resources(:execute => "setpassword")
 end
