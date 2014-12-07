1578eaa43f471e427a57665a5108596532eb91d2
kazuhirosaji <kazuhirosaji@gmail.com>
2014-12-07 12:37:46 +0900

    add crate user command (commentout)

--- cookbooks/mysql/recipes/default.rb
+++ cookbooks/mysql/recipes/default.rb
@@ -7,15 +7,32 @@
 # All rights reserved - Do Not Redistribute
 #
 execute "setpassword" do
-	update = "UPDATE user SET Password='' WHERE Host='localhost' and User='root';"
-	command "sudo /etc/init.d/mysql stop"
-	command "sudo killall mysqld"
-	command "sudo /usr/bin/mysqld_safe -u root --skip-grant-tables &"
-	command "mysqladmin -e #{update}"
-	command "mysqladmin -e FLUSH PRIVILEGES;"
-	command "/usr/bin/mysqladmin -u root password root"
-	action :nothing
+  update = "UPDATE user SET Password='' WHERE Host='localhost' and User='root';"
+  command "sudo /etc/init.d/mysql stop"
+  command "sudo killall mysqld"
+  command "sudo /usr/bin/mysqld_safe -u root --skip-grant-tables &"
+  command "mysqladmin -e #{update}"
+  command "mysqladmin -e FLUSH PRIVILEGES;"
+  command "/usr/bin/mysqladmin -u root password root"
+  action :nothing
 end
+
+# execute "mysql-create-user" do
+#   command "mysql -u root --password=\"#{node['example']['db']['rootpass']}\"  < /tmp/grants.sql"
+#  # /tmp/vagrant-chef-1/chef-solo-1/cookbooks/mysql/templates/default/grants.sql.erb 
+# end
+
+# template "tmp/grants.sql" do
+#   owner "root"
+#   group "root"
+#   mode "0600"
+#   variables(
+#   	:user     => node['example']['db']['user'],
+#   	:password => node['example']['db']['pass'],
+#   	:database => node['example']['db']['database']
+#   )
+#   notifies :run, "execute[mysql-create-user]", :immediately
+# end
 
 package "mysql-server" do
   action :install
