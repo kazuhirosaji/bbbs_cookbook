4286ea554c57b58e2600376accdd9614eccce31b
kazuhirosaji <kazuhirosaji@gmail.com>
2014-12-07 23:25:29 +0900

    mysql create user recipe

---
+++ cookbooks/mysql/attributes/default.rb
@@ -0,0 +1,5 @@
+default['mysql']['db']['user'] = 'saji'
+default['mysql']['db']['pass'] = 'saji'
+default['mysql']['db']['database'] = 'bbbs'
+default['mysql']['db']['host'] = 'localhost'
+default['mysql']['db']['rootpass'] = ''
--- cookbooks/mysql/recipes/default.rb
+++ cookbooks/mysql/recipes/default.rb
@@ -6,31 +6,9 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-execute "setpassword" do
-  update = "UPDATE user SET Password='' WHERE Host='localhost' and User='root';"
-  command "sudo /etc/init.d/mysql stop"
-  command "sudo killall mysqld"
-  command "sudo /usr/bin/mysqld_safe -u root --skip-grant-tables &"
-  command "mysqladmin -e #{update}"
-  command "mysqladmin -e FLUSH PRIVILEGES;"
-  command "/usr/bin/mysqladmin -u root password root"
-  action :nothing
-end
-
-# execute "mysql-create-user" do
-#   command "mysql -u root --password=\"#{node['example']['db']['rootpass']}\"  < /tmp/grants.sql"
-#  # /tmp/vagrant-chef-1/chef-solo-1/cookbooks/mysql/templates/default/grants.sql.erb 
-# end
-
-# template "tmp/grants.sql" do
-#   owner "root"
-#   group "root"
-#   mode "0600"
-#   variables(
-#   	:user     => node['example']['db']['user'],
-#   	:password => node['example']['db']['pass'],
-#   	:database => node['example']['db']['database']
-#   )
+# execute "setpassword" do
+#   # command "/usr/bin/mysqladmin -u root password root"
+#   # action :nothing
 #   notifies :run, "execute[mysql-create-user]", :immediately
 # end
 
@@ -40,5 +18,24 @@ end
 
 service "mysql" do
   action [ :enable, :start]
-  notifies :run, resources(:execute => "setpassword")
+  # notifies :run, resources(:execute => "setpassword")
+end
+
+template "tmp/grants.sql" do
+  source "grants.sql.erb"
+  owner "root"
+  group "root"
+  mode "0600"
+  variables(
+  	:user     => node['mysql']['db']['user'],
+  	:password => node['mysql']['db']['pass'],
+  	:database => node['mysql']['db']['database']
+  )
+  p "call mysql-create-user"
+  notifies :run, "execute[mysql-create-user]", :immediately
+end
+
+execute "mysql-create-user" do
+  p "mysql-create-user !!"
+  command "mysql -u root --password=\"#{node['mysql']['db']['rootpass']}\"  < /tmp/grants.sql"
 end
