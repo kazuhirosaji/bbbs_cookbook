da3df16321f65ede01e181db11f650f15a7123c7
kazuhirosaji <kazuhirosaji@gmail.com>
2014-12-12 08:57:46 +0900

    recipe for php5.4 install

--- cookbooks/php/recipes/default.rb
+++ cookbooks/php/recipes/default.rb
@@ -6,8 +6,45 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-%w{php5 php-pear php5-cli php5-common php5-dev php5-gd php5-mysql libapache2-mod-php5 php-apc}.each do |suffix|
+
+execute "1st apt-get update" do
+  command "apt-get update"
+end
+
+%w{php5 php5-cli php5-common php5-dev php5-gd php5-mysql libapache2-mod-php5 php-pear php-apc}.each do |suffix|
   package suffix do
-    action :install
+ 	action :nothing
+ 	subscribes :install, "execute[1st apt-get update]", :immediately
   end
+end
+
+execute "2nd apt-get update" do
+  command "apt-get update"
+  action :nothing
+  subscribes :run, "package[php-apc]"
+  notifies :install, "package[python-software-properties]", :immediately
+  notifies :upgrade, "package[python-software-properties]", :immediately
+end
+
+package "python-software-properties" do
+  action :nothing
+  notifies :run, "execute[add ppa:ondrej/php5-oldstable]", :immediately
+end
+
+execute "add ppa:ondrej/php5-oldstable" do
+  action :nothing
+  command "add-apt-repository ppa:ondrej/php5-oldstable"
+  notifies :run, "execute[php-reinstall]", :immediately
+end
+
+execute "php-reinstall" do
+  action :run
+  command "apt-get update; apt-get -y install php5"
+  notifies :run, "execute[phpunit-install]", :immediately
+end
+
+execute "phpunit-install" do
+  action :nothing
+  command "pear upgrade PEAR; pear config-set auto_discover 1; pear install pear.phpunit.de/PHPUnit-3.7.32"
+  not_if { ::File.exists?("/usr/bin/phpunit")}
 end
